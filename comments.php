9ac5dac0e2026e3005ffc47340395feddbf46254
Joakim Hertze <joakim.hertze@gmail.com>
2012-07-03 14:14:18 +0200

    Har översatt delar av sökmallen. Har gjort grundläggande css. Har gjort strukturen på index.

--- comments.php
+++ comments.php
@@ -1,12 +1,12 @@
 <?php if ( $comments ) : ?>
   <div id="comments">
-    <h3>Comments</h3>
-    <?php wp_list_comments(); ?>
+    <h2>Kommentarer</h2>
+    <?php wp_list_comments('avatar_size=100'); ?>
     <div class="pagination"><?php paginate_comments_links(); ?></div>
   </div><!-- comments -->
 <?php endif; ?>
 <?php if ( comments_open() || pings_open() ) : ?>
   <?php comment_form( 'comment_notes_before=&comment_notes_after=' ); ?>
 <?php elseif ( $comments ) : ?>
-  <div id="respond"><p id="closed">Comments closed</p></div>
+  <div id="respond"><p id="closed">Kommentarsfunktionen är stängd</p></div>
 <?php endif; ?>
