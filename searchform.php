7d977e3e4c38e4e09aa5b64d888f6021dccaf8b6
Joakim Hertze <joakim.hertze@gmail.com>
2012-07-05 20:33:41 +0200

    Har ersatt asterisker med HTML-kod.

--- searchform.php
+++ searchform.php
@@ -1,6 +1,6 @@
 <form role="search" method="get" id="searchform" action="<?php echo home_url( '/' ); ?>">
     <div>
-        <input type="text" value="" name="s" id="s" placeholder="Leta efter" />
-        <input type="submit" id="searchsubmit" value="Skicka" />
+        <input type="text" value="" name="s" id="s" placeholder="Vad letar du efter?" />
+        <input type="submit" id="searchsubmit" value="Sök" />
     </div>
 </form>
