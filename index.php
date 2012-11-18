a99457a0980dfa95662b75eeb628488dcacacd17
Joakim Hertze <joakim.hertze@gmail.com>
2012-11-18 18:18:58 +0100

    För många ändringar för att kunna sammanfattas.

--- index.php
+++ index.php
@@ -15,8 +15,14 @@
 
   <body>
       <div id="header">
-        <h1><a href="<?php bloginfo('url'); ?>"><?php bloginfo( 'name' ); ?></a></h1>
+        <h1><a href="<?php bloginfo('url'); ?>">✽</a></h1>
         
+        <ul>
+        	<li><a href="/fakta">Fakta</a></li>
+        	<li><a href="/fiktion">Fiktion</a></li>
+        	<li><a href="/kod">Kod</a></li>
+        </ul>
+
       </div>
 
 	<article>
@@ -25,7 +31,7 @@
     	<?php endif; ?>
     	
 		<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-			<div class="post" class="<?php $category = get_the_category(); echo $category[0]->cat_name; ?>" >
+			<div class="<?php if ( is_page()) { echo "page"; } else { echo "post"; } ?>" class="<?php $category = get_the_category(); echo $category[0]->cat_name; ?>" >
 				<?php if ( the_title('','',false)<>'Untitled' ) : ?>
 					<h1><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
 				<?php endif; ?>
@@ -57,7 +63,7 @@
 	        </nav>
         <?php endif; ?>
         <div id="description">
-        	<p><span class="asterisk">&#10045;</span> skrivs av Joakim Hertze och publiceras med Wordpress. Innehåll från <em>Instagram</em> och <em>Readability</em> aggregeras med hjälp av <a href="http://ifttt.com"><em>IFTTT</em></a>. All text är satt i <a href="http://www.myfonts.com/fonts/anatoletype/acuta-medium/"><em>Acuta</em></a>. Kontakta mig via gärna via <span id="obf"><script>document.getElementById("obf").innerHTML="<n uers=\"znvygb:fbpvny@uregmr.pbz\" >rcbfg</n>".replace(/[a-zA-Z]/g,function(c){return String.fromCharCode((c<="Z"?90:122)>=(c=c.charCodeAt(0)+13)?c:c-26);});</script>
+        	<p><span class="asterisk">&#10045;</span> skrivs av Joakim Hertze och publiceras med Wordpress. Innehåll från <em>Instagram</em> och <em>Instapaper</em> samlas in med hjälp av <a href="http://ifttt.com"><em>IFTTT</em></a>. All text är satt i <a href="http://www.myfonts.com/fonts/anatoletype/acuta-medium/"><em>Acuta</em></a>. Kontakta mig via gärna via <span id="obf"><script>document.getElementById("obf").innerHTML="<n uers=\"znvygb:fbpvny@uregmr.pbz\" >rcbfg</n>".replace(/[a-zA-Z]/g,function(c){return String.fromCharCode((c<="Z"?90:122)>=(c=c.charCodeAt(0)+13)?c:c-26);});</script>
 <noscript><span style="unicode-bidi:bidi-override;direction:rtl;">moc.eztreh@laicos</span></noscript></span>, eller <a href="http://twitter.com/hertze">Twitter</a>.</p>
         </div>
         <?php get_search_form(); ?>
